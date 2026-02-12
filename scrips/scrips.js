94c56c1737a5013ee238a317f302a83c2d0b9e59
Nazif Rahiq <nazif.rahiq@edu-kw1c.nl>
2026-02-12 23:06:52 +0100

    Eerste commit

---
+++ scrips/scrips.js
@@ -0,0 +1,33 @@
+/*
+Naam: Nazif Rahiq
+Datum: 9-1-2026
+
+File: Random
+*/
+
+var buttonElement = document.getElementById("buttonKlikMij");
+
+buttonElement.addEventListener("click",
+function()
+{
+alert("Je hebt op me geklikt!");
+});
+
+var stopLichtKleur = "Rood";
+
+if (stopLichtKleur == "Rood")
+{
+    console.log("Je moet stoppen");
+}
+else if(stopLichtKleur == "Oranje")
+{
+    console.log("Je moet oppassen");
+}
+else if (stopLichtKleur == "Groen")
+{
+    console.log("Je mag doorrijden!");
+}
+
+
+
+
